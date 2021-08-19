4677135a57a581cdc44de6334f737a2a033ac9f1
xiaoqch <37901097+xiaoqch@users.noreply.github.com>
2021-08-19 17:07:30 +0800

    Delete TestSym directory

--- TestSym/SymHelper.h
+++
@@ -1,7 +0,0 @@
-#include "pch.h"
-#include "mc/BlockSource.h"
-#include "mc/LevelChunk.h"
-
-BlockSource* getBlockSourceByDim(int dimid);
-LevelChunk* getChunkByXZ(BlockSource* bs, int x, int z);
-void setChunkUnsaved(LevelChunk* lc);
