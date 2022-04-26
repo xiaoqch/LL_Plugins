e6169342e842a4fa3b0698a14950c27339c71913
xiaoqch <xiaoqch@foxmail.com>
2022-04-27 00:44:50 +0800

    .

--- FixUpgradeActorCrash/Main.cpp
+++ FixUpgradeActorCrash/Main.cpp
@@ -16,33 +16,6 @@ void entry()
 #include <MC/Dimension.hpp>
 #ifdef DEBUG
 
-TInstanceHook(bool, "?initialize@Level@@UEAA_NAEBV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@AEBVLevelSettings@@PEAVLevelData@@AEBVExperiments@@PEBV23@@Z",
-              Level, std::string const& a1, class LevelSettings const& a2, class LevelData* a3, class Experiments const& a4, std::string const* a5)
-{
-    auto rtn = original(this, a1, a2, a3, a4, a5);
-    static DBHelpers::Category category = (DBHelpers::Category)4;
-    std::string backupDir = fmt::format("{}{:%Y%m%d-%H%M%S}/", PLUGIN_DIR, fmt::localtime(_time64(nullptr)));
-    bool first = true;
-    Global<DBStorage>->forEachKeyWithPrefix("actorprefix", category, [&backupDir,&first](gsl::cstring_span<-1> suffix, gsl::cstring_span<-1> data) {
-        if (data.size() > Config::SizeThreshold)
-        {
-            if (first) {
-                std::filesystem::create_directories(std::filesystem::path(backupDir));
-                first = false;
-            }
-            std::string suffixStr = std::string(suffix.data(), suffix.size());
-            std::string suffixHex = Util::toHex(suffixStr);
-            std::string fullKey = "actorprefix" + suffixStr;
-            auto backupFilePath = fmt::format("{}actorprefix-{}.bin", backupDir, suffixHex);
-            logger.warn("Remove Data: key: actorprefix-{}, size: {}", suffixHex, data.size());
-            logger.warn("backup path: {}", backupFilePath);
-            std::string sdata = std::string(data.data(), data.size());
-            WriteAllFile(backupFilePath, sdata, true);
-            Global<DBStorage>->deleteData(fullKey, category);
-        }
-    });
-    return rtn;
-}
 #include <MC/DBStorage.hpp>
 
 TInstanceHook(bool, "?loadData@DBStorage@@UEBA_NV?$basic_string_span@$$CBD$0?0@gsl@@AEAV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@W4Category@DBHelpers@@@Z",
@@ -138,6 +111,14 @@ inline std::string& fixActorKeys(class LevelChunk& chunk, std::string& keys)
         keys.append(fixedKey);
     }
     Global<DBStorage>->saveData(digKey, std::string(keys), (DBHelpers::Category)4);
+    static int tickingAreaIndex = 0;
+    std::string id = fmt::format("FixActor-{}", ++tickingAreaIndex);
+    auto& cpos = chunk.getPosition();
+    Level::executeCommand(fmt::format("tickingarea add {} {} {} {} {} {} {}",
+        (cpos.x - 1) * 16, 0, (cpos.z - 1) * 16, (cpos.x + 1) * 16, 0, (cpos.z + 1) * 16, id));
+    Schedule::delay([id]() {
+        Level::executeCommand(fmt::format("tickingarea remove {}", id));
+    }, 5);
     return keys;
 }
 
@@ -177,6 +158,8 @@ TClasslessInstanceHook(std::string&, "?_getEntityDigestKey@DBChunkStorage@@AEBA?
     auto& rtn = original(this, res, a1);
     std::string digestData;
     Global<DBStorage>->loadData(rtn, digestData, (DBHelpers::Category)4);
+    if (digestData.size() / 8 > 200)
+        __debugbreak();
     return rtn;
 }
 
