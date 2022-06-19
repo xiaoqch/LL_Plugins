f06d998dbd5c374586a0c7b3f6001963d65c948c
xiaoqch <xiaoqch@foxmail.com>
2022-06-19 16:53:30 +0800

    Update ability pro

--- AbilityPro/AbilityProCommand.cpp
+++ AbilityPro/AbilityProCommand.cpp
@@ -3,23 +3,12 @@
 //#include <MC/Abilities.hpp>
 //#include <MC/Ability.hpp>
 #include <MC/Packet.hpp>
+#include <MC/SynchedActorData.hpp>
 #include <MC/Level.hpp>
 #include <third-party/magic_enum/magic_enum.hpp>
 
 using namespace RegisterCommandHelper;
 
-#ifdef DEBUG
-struct AdventureSettings
-{
-    bool mNoPvM;
-    bool mNoMvP;
-    bool mWorldImmutable;
-    bool mShowNameTags;
-    bool mAutoJump;
-
-public:
-    MCAPI AdventureSettings();
-};
 enum class AdventureFlag : int
 {
     WorldImmutable = 1,
@@ -33,6 +22,18 @@ enum class AdventureFlag : int
     WorldBuilder = 256,
     Flying = 512,
     Muted = 1024,
+};
+#ifdef DEBUG
+struct AdventureSettings
+{
+    bool mNoPvM;
+    bool mNoMvP;
+    bool mWorldImmutable;
+    bool mShowNameTags;
+    bool mAutoJump;
+
+public:
+    MCAPI AdventureSettings();
 };
 
 enum class ActionPermission : int
@@ -129,6 +130,29 @@ public:
 #include <MC/Abilities.hpp>
 #endif // DEBUG
 
+class RequestAbilityPacket : public Packet
+{
+public:
+    AbilitiesIndex mAbility = (AbilitiesIndex)-1;
+    bool mHasValue = false;
+    char filler51;
+    bool mValue = false;
+    char filler53[3];
+    bool unk56 = false;
+
+
+public:
+    /*0*/ virtual ~RequestAbilityPacket();
+    /*1*/ virtual enum MinecraftPacketIds getId() const;
+    /*2*/ virtual std::string getName() const;
+    /*3*/ virtual void write(class BinaryStream&) const;
+    /*6*/ virtual enum StreamReadResult _read(class ReadOnlyBinaryStream&);
+
+    MCAPI RequestAbilityPacket(enum AbilitiesIndex, bool);
+    MCAPI RequestAbilityPacket();
+    MCAPI enum AbilitiesIndex getAbility() const;
+    MCAPI bool tryGetBool(bool&) const;
+};
 class AdventureSettingsPacket : public Packet
 {
 public:
@@ -179,49 +203,53 @@ inline void sendNetworkPacket(Player& player, class Packet& a0)
     return (player.*rv)(std::forward<class Packet&>(a0));
 }
 
+void _updateAbility(Player& player, AbilitiesIndex index, bool value)
+{
+}
+
 void setPlayerAbility(Player& player, AbilitiesIndex index, bool value)
 {
     ActorUniqueID uid = player.getUniqueID();
-    auto abilities2 = getPlayerAbilities(uid);
+    //auto abilities = Global<Level>->getPlayerAbilities(uid);
     auto abilities = &dAccess<Abilities>(&player, 2512);
 #ifdef DEBUG
     auto map = abilities->toMap();
 #endif // DEBUG
+    auto flying = abilities->getAbility(AbilitiesIndex::Flying).getBool();
     if (index == AbilitiesIndex::Flying && value && player.isOnGround())
     {
         abilities->setAbility(AbilitiesIndex::MayFly, value);
-        abilities2->setAbility(AbilitiesIndex::MayFly, value);
     }
-    if (index == AbilitiesIndex::NoClip) {
-        //abilities->setAbility(AbilitiesIndex::MayFly, value);
-        //abilities->setAbility(AbilitiesIndex::Flying, value);
+    if (index == AbilitiesIndex::MayFly && value == false && flying)
+    {
+        abilities->setAbility(AbilitiesIndex::Flying, false);
     }
     abilities->setAbility(index, value);
-    abilities2->setAbility(index, value);
     auto mayfly = abilities->getAbility(AbilitiesIndex::MayFly).getBool();
-    auto mayfly2 = abilities2->getAbility(AbilitiesIndex::MayFly).getBool();
     auto noclip = abilities->getAbility(AbilitiesIndex::NoClip).getBool();
-    auto noclip2 = abilities2->getAbility(AbilitiesIndex::NoClip).getBool();
-    if (mayfly || mayfly2 || noclip || noclip2)
+    player.setCanFly(mayfly || noclip);
+    if (index == AbilitiesIndex::NoClip)
     {
-        player.setCanFly(true);
-    }
-    else
-    {
-        player.setCanFly(false);
+        abilities->setAbility(AbilitiesIndex::Flying, value);
     }
 #ifdef DEBUG
     auto map2 = abilities->toMap();
 #endif // DEBUG
+    flying = abilities->getAbility(AbilitiesIndex::Flying).getBool();
     AdventureSettingsPacket pkt(getAdventureSettings(), *abilities, uid, false);
-    sendNetworkPacket(player, pkt);
+    
+    pkt.mFlag &= ~static_cast<unsigned int>(AdventureFlag::Flying);
+    if (flying)
+        pkt.mFlag |= static_cast<unsigned int>(AdventureFlag::Flying);
     player._sendDirtyActorData();
+    sendNetworkPacket(player, pkt);
 }
 
 bool getPlayerAbility(Player& player, AbilitiesIndex index)
 {
-    ActorUniqueID uid = player.getUniqueID();
-    auto abilities = getPlayerAbilities(uid);
+    //ActorUniqueID uid = player.getUniqueID();
+    //auto abilities = getPlayerAbilities(uid);
+    auto abilities = &dAccess<Abilities>(&player, 2512);
     return abilities->getAbility(index).getBool();
 }
 
@@ -233,7 +261,6 @@ void AbilityProCommand::execute(class CommandOrigin const& origin, class Command
     }
     std::string names = "";
     if (!mIndex_isSet) {
-
         return output.success("build, mine, doorandswitches, opencontainers, attackplayers, attackmobs, op, teleport, invulnerable, flying, mayfly, instabuild, lightning, mute, worldbuilder, noclip");
     }
     if (!mSwitch_isSet)
@@ -257,7 +284,7 @@ void AbilityProCommand::execute(class CommandOrigin const& origin, class Command
     else
     {
         names = names.substr(2);
-        output.success(fmt::format("Success to set ability \"{}\" to {} for {}", magic_enum::enum_name(mIndex), mSwitch, names));
+        output.success(fmt::format("Success to set ability \"{}\" {} for {}", magic_enum::enum_name(mIndex), mSwitch, names));
     }
 }
 
@@ -289,28 +316,6 @@ void AbilityProCommand::setup(CommandRegistry& registry)
             //{"worldbuilder", AbilitiesIndex::WorldBuilder},
             {"noclip", AbilitiesIndex::NoClip},
         });
-    registry.addEnum<AbilitiesIndex>(
-        "Ability",
-        {
-            {"build", AbilitiesIndex::Build},
-            {"mine", AbilitiesIndex::Mine},
-            {"doorandswitches", AbilitiesIndex::DoorAndSwitches},
-            {"opencontainers", AbilitiesIndex::OpenContainers},
-            {"attackplayers", AbilitiesIndex::AttackPlayers},
-            {"attackmobs", AbilitiesIndex::AttackMobs},
-            //{"op", AbilitiesIndex::Op},
-            {"teleport", AbilitiesIndex::Teleport},
-            {"invulnerable", AbilitiesIndex::Invulnerable},
-            {"flying", AbilitiesIndex::Flying},
-            {"mayfly", AbilitiesIndex::MayFly},
-            {"instabuild", AbilitiesIndex::InstaBuild},
-            {"lightning", AbilitiesIndex::Lightning},
-            //{"FlySpeed", AbilitiesIndex::FlySpeed},
-            //{"WalkSpeed", AbilitiesIndex::WalkSpeed},
-            {"mute", AbilitiesIndex::Mute},
-            //{"worldbuilder", AbilitiesIndex::WorldBuilder},
-            {"noclip", AbilitiesIndex::NoClip},
-        });
 
     auto playerParam = makeMandatory<AbilityProCommand>(&AbilityProCommand::mPlayer, "target");
     auto indexParam = makeMandatory<CommandParameterDataType::ENUM, AbilityProCommand>(&AbilityProCommand::mIndex, "ability", "AbilityPro", &AbilityProCommand::mIndex_isSet);
@@ -320,21 +325,67 @@ void AbilityProCommand::setup(CommandRegistry& registry)
     registry.registerOverload<AbilityProCommand>("abilitypro", playerParam, indexParam, valueParam);
     registry.registerOverload<AbilityProCommand>("abilitypro", playerParam, indexOptional);
 }
-
+#include <MC/ServerPlayer.hpp>
+TInstanceHook(void, "?handle@ServerNetworkHandler@@UEAAXAEBVNetworkIdentifier@@AEBVRequestAbilityPacket@@@Z",
+              ServerNetworkHandler, class NetworkIdentifier const& nid, class RequestAbilityPacket const& pkt)
+{
+    original(this, nid, pkt);
+    auto index = pkt.getAbility();
+    if (index == AbilitiesIndex::Flying)
+    {
+        auto sp = _getServerPlayer(nid, pkt.clientSubId);
+        if (!sp)
+            return;
+        if (!sp->getUserEntityIdentifierComponent())
+            return;
+        bool flying;
+        if (!pkt.tryGetBool(flying))
+            return;
+        auto abilities = &dAccess<Abilities>(sp, 2512);
+        auto mayFly = abilities->getAbility(AbilitiesIndex::MayFly).getBool();
+        flying = flying && mayFly;
+        AdventureSettingsPacket pkt(getAdventureSettings(), *abilities, sp->getUniqueID(), false);
+        pkt.mFlag &= ~static_cast<unsigned int>(AdventureFlag::Flying);
+        if (flying)
+            pkt.mFlag |= static_cast<unsigned int>(AdventureFlag::Flying);
+        abilities->setAbility(AbilitiesIndex::Flying, flying);
+        sp->sendNetworkPacket(pkt);
+        DEBUGW("{} set may flying -> {}", getName(), flying);
+    }
+}
 #ifdef DEBUG
 
 TInstanceHook(AdventureSettingsPacket&, "??0AdventureSettingsPacket@@QEAA@AEBUAdventureSettings@@AEBVAbilities@@UActorUniqueID@@_N@Z",
               AdventureSettingsPacket, struct AdventureSettings const& settings, class Abilities const& abilities, struct ActorUniqueID uniqueId, bool unk_0)
 {
     auto map = abilities.toMap();
-    return original(this, settings, abilities, uniqueId, unk_0);
+    auto&pkt = original(this, settings, abilities, uniqueId, unk_0);
+    if (abilities.getAbility(AbilitiesIndex::MayFly).getBool() && abilities.getAbility(AbilitiesIndex::Flying).getBool()) {
+        pkt.mFlag |= (int)AdventureFlag::Flying;
+    }
+    return pkt;
 }
 TInstanceHook(bool, "?canUseAbility@Player@@QEBA_NW4AbilitiesIndex@@@Z",
               Player, enum AbilitiesIndex index)
 {
     auto rtn = original(this, index);
     DEBUGW("{} {} use ability {}", getName(), rtn ? "can" : "can not", magic_enum::enum_name(index));
-    return true;
+    return rtn;
 }
+TClasslessInstanceHook(bool, "?isFlying@PlayerMovement@@YA_NAEBUIPlayerMovementProxy@@@Z",
+              struct IPlayerMovementProxy const& a1)
+{
+    auto rtn = original(this, a1);
+    DEBUGW("isFlying: {}", rtn);
+    return rtn;
+}
+//TInstanceHook(bool, "?isFlying@Abilities@@QEBA_NXZ",
+//              Abilities)
+//{
+//    //auto rtn = original(this);
+//    auto rtn = this->getAbility(AbilitiesIndex::Flying).getBool();
+//    DEBUGW("Abilities::isFlying: {}", rtn);
+//    return true;
+//}
 
 #endif // DEBUG
